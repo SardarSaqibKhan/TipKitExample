d84ba4da4dc6c8597fc127496eed04b328450cdc
Sardar Saqib <44336686+SardarSaqibKhan@users.noreply.github.com>
2025-03-21 14:06:15 +0500

    setup project

---
+++ TipKitExample/TipKitExample/TipKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitExampleApp.swift
+//  TipKitExample
+//
+//  Created by sardar saqib on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TipKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
